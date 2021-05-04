e77cddff2490904482ac620248ce5065be9316fa
unknown <Bloodman@post.cz>
2021-05-04 17:43:57 +0200

    8

--- Digital-electronics-1-master/Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
+++ Digital-electronics-1-master/Labs/08-traffic_lights/traffic/traffic.srcs/sources_1/new/tlc.vhd
@@ -1,22 +1,103 @@
-# H1 Digital-electronics-1 
-## 08-Trafic Lights
-## 1. Přírava
+------------------------------------------------------------------------
+--
+-- Traffic light controller using FSM.
+-- Nexys A7-50T, Vivado v2020.1.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+-- This code is inspired by:
+-- [1] LBEbooks, Lesson 92 - Example 62: Traffic Light Controller
+--     https://www.youtube.com/watch?v=6_Rotnw1hFM
+-- [2] David Williams, Implementing a Finite State Machine in VHDL
+--     https://www.allaboutcircuits.com/technical-articles/implementing-a-finite-state-machine-in-vhdl/
+-- [3] VHDLwhiz, One-process vs two-process vs three-process state machine
+--     https://vhdlwhiz.com/n-process-state-machine/
+--
+------------------------------------------------------------------------
 
-| **Input P** | `0` | `0` | `1` | `1` | `0` | `1` | `0` | `1` | `1` | `1` | `1` | `0` | `0` | `1` | `1` | `1` |
-| :-- | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: |
-| **Clock** | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ |
-| **State** | A | A | B | C | C | D | A | B | C | D | B | B | B | C | D | B |
-| **Output R** | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 1 | 0 | 0 | 0 | 0 | 1 | 0 |
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-| **RGB LED** | **Artix-7 pin names** | **Red** | **Yellow** | **Green** |
-| :-: | :-: | :-: | :-: | :-: |
-| LD16 | N15, M16, R12 | `1,0,0` | `1,1,0` | `0,1,0` |
-| LD17 | N16, R11, G14 | `1,0,0` | `1,1,0` | `0,1,0` |
+------------------------------------------------------------------------
+-- Entity declaration for traffic light controller
+------------------------------------------------------------------------
+entity tlc is
+    port(
+        clk     : in  std_logic;
+        reset   : in  std_logic;
+        -- Traffic lights (RGB LEDs) for two directions
+        south_o : out std_logic_vector(3 - 1 downto 0);
+        west_o  : out std_logic_vector(3 - 1 downto 0)
+    );
+end entity tlc;
 
-<img src="traffic.png">
+------------------------------------------------------------------------
+-- Architecture declaration for traffic light controller
+------------------------------------------------------------------------
+architecture Behavioral of tlc is
 
-````vhdl
-p_traffic_fsm : process(clk)
+    -- Define the states
+    type t_state is (STOP1,
+                     WEST_GO,
+                     WEST_WAIT,
+                     STOP2,
+                     SOUTH_GO,
+                     SOUTH_WAIT);
+    type sm_state is (STOP1,
+                     WEST_GO,
+                     WEST_WAIT,
+                     STOP2,
+                     SOUTH_GO,
+                     SOUTH_WAIT,
+                     BOTH_CARS1,
+                     CARS_WAIT1,
+                     BOTH_CARS2,
+                     CARS_WAIT2);
+    -- Define the signal that uses different states
+    signal s_state  : t_state;
+    signal s_smart_state : sm_state;
+
+    -- Internal clock enable
+    signal s_en     : std_logic;
+    signal s_car_south : std_logic;
+    signal s_car_west : std_logic;
+    -- Local delay counter
+    signal   s_cnt  : unsigned(5 - 1 downto 0);
+
+    -- Specific values for local counter
+    constant c_DELAY_4SEC : unsigned(5 - 1 downto 0) := b"1_0000";
+    constant c_DELAY_2SEC : unsigned(5 - 1 downto 0) := b"0_1000";
+    constant c_DELAY_1SEC : unsigned(5 - 1 downto 0) := b"0_0100";
+    constant c_ZERO       : unsigned(5 - 1 downto 0) := b"0_0000";
+
+begin
+
+    --------------------------------------------------------------------
+    -- Instance (copy) of clock_enable entity generates an enable pulse
+    -- every 250 ms (4 Hz). Remember that the frequency of the clock 
+    -- signal is 100 MHz.
+    
+    -- JUST FOR SHORTER/FASTER SIMULATION
+    s_en <= '1';
+--    clk_en0 : entity work.clock_enable
+--        generic map(
+--            g_MAX =>        -- g_MAX = 250 ms / (1/100 MHz)
+--        )
+--        port map(
+--            clk   => clk,
+--            reset => reset,
+--            ce_o  => s_en
+--        );
+
+    --------------------------------------------------------------------
+    -- p_traffic_fsm:
+    -- The sequential process with synchronous reset and clock_enable 
+    -- entirely controls the s_state signal by CASE statement.
+    --------------------------------------------------------------------
+    p_traffic_fsm : process(clk)
     begin
         if rising_edge(clk) then
             if (reset = '1') then       -- Synchronous reset
@@ -102,50 +183,8 @@ p_traffic_fsm : process(clk)
             end if; -- Synchronous reset
         end if; -- Rising edge
     end process p_traffic_fsm;
-````
-
-````vhdl
- p_output_fsm : process(s_state)
-    begin
-        case s_state is
-            when STOP1 =>
-                south_o <= "100";   -- Red (RGB = 100)
-                west_o  <= "100";   -- Red (RGB = 100)
-            when WEST_GO =>
-                south_o <= "100";   
-                west_o  <= "010";
-            when WEST_WAIT =>
-                south_o <= "100";
-                west_o  <= "110";
-            when STOP2 =>
-                south_o <= "100";
-                west_o  <= "100";
-            when SOUTH_GO =>
-                south_o <= "010";
-                west_o  <= "100";
-            when SOUTH_WAIT =>
-                south_o <= "110";
-                west_o  <= "100";
-            when others =>
-                south_o <= "100";   -- Red
-                west_o  <= "100";   -- Red
-        end case;
-    end process p_output_fsm;
-````
-<img src="traffic_wide.png">
-<img src="traffic_detail.png">
-
-
-| **Clock** | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ | ^ |
-| :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | :-: | 
-| **Car North** | `0` | `0` | `0` | `1` | `0` | `0` | `1` | `0` | `0` | `1` | `0` | `0` |
-| **Car East**  | `1` | `0` | `0` | `0` | `0` | `0` | `1` | `0` | `0` | `1` | `0` | `0` | 
-| **State** | STOP1 | WEST GO | WEST WAIT | STOP2 | SOUTH GO | SOUTH WAIT | STOP1 | BOTH CARS 1 | CARS WAIT 1 | STOP2 | BOTCH CARS 2 | CARS WAIT 2 |
-
-<img src="smart traffic.png">
-
-````vhdl
-p_smart_traffic_fsm : process(clk)
+    
+    p_smart_traffic_fsm : process(clk)
     begin
         if rising_edge(clk) then
             if (reset = '1') then       -- Synchronous reset
@@ -235,4 +274,38 @@ p_smart_traffic_fsm : process(clk)
             end if; -- Synchronous reset
         end if; -- Rising edge
     end process p_smart_traffic_fsm;
-````
+
+    --------------------------------------------------------------------
+    -- p_output_fsm:
+    -- The combinatorial process is sensitive to state changes, and sets
+    -- the output signals accordingly. This is an example of a Moore 
+    -- state machine because the output is set based on the active state.
+    --------------------------------------------------------------------
+    p_output_fsm : process(s_state)
+    begin
+        case s_state is
+            when STOP1 =>
+                south_o <= "100";   -- Red (RGB = 100)
+                west_o  <= "100";   -- Red (RGB = 100)
+            when WEST_GO =>
+                south_o <= "100";   
+                west_o  <= "010";
+            when WEST_WAIT =>
+                south_o <= "100";
+                west_o  <= "110";
+            when STOP2 =>
+                south_o <= "100";
+                west_o  <= "100";
+            when SOUTH_GO =>
+                south_o <= "010";
+                west_o  <= "100";
+            when SOUTH_WAIT =>
+                south_o <= "110";
+                west_o  <= "100";
+            when others =>
+                south_o <= "100";   -- Red
+                west_o  <= "100";   -- Red
+        end case;
+    end process p_output_fsm;
+
+end architecture Behavioral;
