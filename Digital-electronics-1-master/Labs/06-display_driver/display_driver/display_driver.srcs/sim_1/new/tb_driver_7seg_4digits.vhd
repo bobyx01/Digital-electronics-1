2087e34f9fbc3016c3ad8890427d1b7eb58d22c1
unknown <Bloodman@post.cz>
2021-05-04 17:30:51 +0200

    6

--- Digital-electronics-1-master/Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
+++ Digital-electronics-1-master/Labs/06-display_driver/display_driver/display_driver.srcs/sim_1/new/tb_driver_7seg_4digits.vhd
@@ -1,36 +1,17 @@
-# H1 Digital-electronics-1 
-## 06-display_driver
+------------------------
+library ieee;
+use ieee.std_logic_1164.all;
 
-<img src="diagram.png">
+------------------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------------------
+entity tb_driver_7seg_4digits is
+    -- Entity of testbench is always empty
+end entity tb_driver_7seg_4digits;
 
-``` VHDL
-    p_mux : process(s_cnt, data0_i, data1_i, data2_i, data3_i, dp_i)
-    begin
-        case s_cnt is
-            when "11" =>
-                s_hex <= data3_i;
-                dp_o  <= dp_i(3);
-                dig_o <= "0111";
-
-            when "10" =>
-                s_hex <= data2_i;
-                dp_o  <= dp_i(2);
-                dig_o <= "1011";
-
-            when "01" =>
-                s_hex <= data1_i;
-                dp_o  <= dp_i(1);
-                dig_o <= "1101";
-
-            when others =>
-                s_hex <= data0_i;
-                dp_o  <= dp_i(0);
-                dig_o <= "1110";
-        end case;
-    end process p_mux;
-```
-
-``` VHDL
+------------------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------------------
 architecture testbench of tb_driver_7seg_4digits is
 
     -- Local constants
@@ -111,13 +92,14 @@ uut_driver_7seg : entity work.driver_7seg_4digits
         s_data0_i <= "0010";
         s_dp_i    <= "0111";
         
+             
         wait for 500 ns;
-        s_data3_i <= "0001";
-        s_data2_i <= "0010";
-        s_data1_i <= "0011";
-        s_data0_i <= "0100";
+        s_data3_i <= "0001"; -- 1
+        s_data2_i <= "0010"; -- 2
+        s_data1_i <= "0011"; -- 3
+        s_data0_i <= "0100"; -- 4
         
-  -- assert segment digit = 2
+          -- assert segment digit = 2
         assert(s_seg_o = "0010010")
         report "First segment wrong digit." severity note;
         
@@ -135,67 +117,13 @@ uut_driver_7seg : entity work.driver_7seg_4digits
   -- assert decimal point
         assert(s_dp_o = '0')
         report "Decimal point on wrong digit." severity note;
-        
   -- assert segment digit = 3
         assert(s_seg_o = "0000110")
         report "Last segment wrong digit." severity note;
+
+        
         report "Stimulus process finished" severity note;
         wait;
     end process p_stimulus;
 
 end architecture testbench;
-```
-<img src="simulation.png">
-
-``` VHDL
-architecture Behavioral of top is
-
-begin
-    -- Instance (copy) of driver_7seg_4digits entity
-    driver_seg_4 : entity work.driver_7seg_4digits
-        port map(
-            clk        => CLK100MHZ,
-            reset      => BTNC,
-            data0_i(3) => SW(3),
-            data0_i(2) => SW(2),
-            data0_i(1) => SW(1),
-            data0_i(0) => SW(0),
-            
-            data1_i(3) => SW(7),
-            data1_i(2) => SW(6),
-            data1_i(1) => SW(5),
-            data1_i(0) => SW(4),
-            
-            data2_i(3) => SW(11),
-            data2_i(2) => SW(10),
-            data2_i(1) => SW(9),
-            data2_i(0) => SW(8),
-            
-            data3_i(3) => SW(15),
-            data3_i(2) => SW(14),
-            data3_i(1) => SW(13),
-            data3_i(0) => SW(12),
-            
-            dig_o => AN(4-1 downto 0),
-            --- WRITE YOUR CODE HERE
-            
-            seg_o(6) => CA,
-            seg_o(5) => CB,
-            seg_o(4) => CC,
-            seg_o(3) => CD,
-            seg_o(2) => CE,
-            seg_o(1) => CF,
-            seg_o(0) => CG,
-            
-            dp_i => "0111",
-            dp_o => DP
-            
- );
-
-    -- Disconnect the top four digits of the 7-segment display
-    AN(7 downto 4) <= b"1111";
-
-end architecture Behavioral;
-```
-
-<img src="driver_schematic.jpg">
